be38f69f3be70b42d9c0f5b8d7114bb47055093c
Craig Satchell <satchdev@gmail.com>
2021-07-13 19:31:26 -0400

    def state variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 const App = () => {
+   const [currentVideo, setCurrentVideo] = useState('');
+   const [relatedVideos, setRelatedVideos] = useState([]);
+   const [newComment, setNewComment] = useState('');
+   const [newReply, setNewReply] = useState('');
+   const [likes, setLikes] = useState('');
+   const [dislikes, setDislikes] = useState('');
+   const [searchText, setSearchText] = useState('');
+
+   useEffect(() => {
+      
+   })
+
    return (
       <div id='app'>
          <h1>YouTube Clone</h1>
