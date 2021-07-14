0fb5b7fb47254dce7f1b9f10a6da2e534a7c5709
Craig Satchell <satchdev@gmail.com>
2021-07-13 21:12:30 -0400

    test youtube api axios call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
-import apiKey from './API/apiKey';
+import apiKey from './api/apiKey';
 
 const App = () => {
-   const [currentVideo, setCurrentVideo] = useState('');
+   const [currentVideo, setCurrentVideo] = useState({});
    const [relatedVideos, setRelatedVideos] = useState([]);
    const [newComment, setNewComment] = useState('');
    const [newReply, setNewReply] = useState('');
    const [likes, setLikes] = useState('');
    const [dislikes, setDislikes] = useState('');
    const [searchText, setSearchText] = useState('');
-   const [allVideos, setAllVideos] = useState([]);
+   const [allVideos, setAllVideos] = useState({});
 
-   useEffect(() => {
-      axios.get(`https://www.googleapis.com/youtube/v3/search?q="HarryPotter&key=${apiKey}`)
-      .then(response => setAllVideos(response.data.items))
-   }, [allVideos]);
+
+   // useEffect(() => {
+   //    axios.get(`https://www.googleapis.com/youtube/v3/search?q=harrypotter&key=${apiKey}`).then(response => console.log(response.data)).catch(err => { console.log(err); })  })
+
+   // }, [currentVideo]);
+
+
    
-   console.log(allVideos);
+   useEffect(() => {
+      (async () => {
+         await axios.get(`https://www.googleapis.com/youtube/v3/search?q=harrypotter&key=${apiKey}`).then(response => console.log(response.data)).catch(err => console.log(err))
+      })()
+   }, []);
+
+
+   //    (async () => {
+   //       await axios.get(`https://www.googleapis.com/youtube/v3/search?q=harrypotter&key=${apiKey}`).then(response => console.log(response.data)).catch(err => console.log(err))
+   //   })()
+
 
    return (
       <div id='app'>
