2fb9d0d18f38ee875c4b8d2d48fe924c120a873d
Craig Satchell <satchdev@gmail.com>
2021-07-16 18:40:05 -0400

    minor css layout changes

--- src/components/relatedVideos/listRelatedVideos/ListRelatedVideos.js
+++ src/components/relatedVideos/listRelatedVideos/ListRelatedVideos.js
@@ -3,19 +3,8 @@ import ViewRelatedVideo from '../viewRelatedVideo/ViewRelatedVideo';
 import './ListRelatedVideos.css';
 
 const ListRelatedVideos = (props) => {
-   // let listItem;
-   // if (!props.relatedVideos) {
-   //    listItem = props.relatedVideos.map((video) => {
-   //       return (<li key={video.id.videoId}>
-   //          <ViewRelatedVideo video={video} />
-   //       </li>)
-   //    })
-   // }
-   console.log('videos:',props.relatedVideos);
-
    return (
       <div id='list-related-videos'>
-         <h4>Related Videos</h4>
          <ul>
             {props.relatedVideos ? props.relatedVideos.map((video) => {
                return (<li key={video.id.videoId}>
@@ -27,9 +16,5 @@ const ListRelatedVideos = (props) => {
    )
 }
 
-// Ally Notes. 
-// Use Cards for Videos. 
-// Image onClick - would need to pass in ....item[i].id.videoID (based on postman return)
-// Image thumbnail from postman would include img src of ......item[i].snippet.thumbnails.default.url (based on postman return)
 
 export default ListRelatedVideos
