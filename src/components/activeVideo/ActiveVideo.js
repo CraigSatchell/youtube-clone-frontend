6f90b6b785b3829361ee7ead329696fe4cef94f5
Craig Satchell <satchdev@gmail.com>
2021-07-17 02:32:51 -0400

    fixed missing snippet issue ActiveVideo  component

--- src/components/activeVideo/ActiveVideo.js
+++ src/components/activeVideo/ActiveVideo.js
@@ -6,7 +6,7 @@ const ActiveVideo = (props) => {
    return (
       <div id='active-video'>
          {props.currentVideo ? <iframe id="ytplayer" title='currentvideo' width="100%" height="500px" src={`https://www.youtube.com/embed/${props.currentVideo.id.videoId}`}></iframe> : 'Loading . . .'}
-         {props.currentVideo ? <div><h2>{props.currentVideo.snippet.title}</h2> <p> {props.currentVideo.snippet.description}</p></div> : 'Loading . . . '}
+         {props.currentVideo ? props.currentVideo.snippet ? <div><h2>{props.currentVideo.snippet.title}</h2> <p> {props.currentVideo.snippet.description}</p></div> : '' : ''}
       </div>
    )
 }
