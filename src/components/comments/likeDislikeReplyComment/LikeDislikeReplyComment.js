bcff49f269d60bc252eafba79aec11f2d6144e8e
Craig Satchell <satchdev@gmail.com>
2021-07-18 02:04:13 -0400

    mod LikeDislikeReplyComments comp.

--- src/components/comments/likeDislikeReplyComment/LikeDislikeReplyComment.js
+++ src/components/comments/likeDislikeReplyComment/LikeDislikeReplyComment.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReplyComment from '../replyComment/ReplyComment';
+import { ThumbDownAlt } from '@material-ui/icons';
+import { ThumbUpAlt } from '@material-ui/icons';
+import {  } from '@material-ui/icons';
 import './LikeDislikeReplyComment.css';
 
 const LikeDislikeComment = (props) => {
    return (
       <div className='like-dislike-comment'>
-         <button onClick={props.handleLike}>Like</button>{props.comment.likes}
-         <button onClick={props.handleDislike}>Dislike </button>{props.comment.dislikes}
+         <button onClick={props.handleLike}><ThumbUpAlt /></button>{props.comment.likes}
+         <button onClick={props.handleDislike}><ThumbDownAlt /></button>{props.comment.dislikes}
          <button onClick={() => props.handleReplyCommentClick(props.comment._id)}>Reply</button>
          <div id={props.comment._id} className="hidden"><ReplyComment /></div>
       </div>
